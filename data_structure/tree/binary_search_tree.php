f059b2b1e183c9ed65ac6ce4638ba18d3891abe8
Jon Bevan <jon@jonbevan.me.uk>
2012-06-25 14:31:01 +0100

    Removed unnecessary draw functions

--- data_structure/tree/binary_search_tree.php
+++ data_structure/tree/binary_search_tree.php
@@ -129,23 +129,6 @@ class Node {
   	return $maxDepth;
   }
 
-
-  public function draw(&$render, &$val_width = 0, &$col = 0, $row = 0) {
-  	if (strlen($this->_val) > $val_width) {
-  		$val_width = strlen($this->_val);
-  	}
-  	$col = $col*2 + 1;
-  	if ($this->_left->val() !== null) {
-  		$this->_left->draw($render, $val_width, $col, $row+1);
-  	}
-//  	$col--;// = ($col-1) / 2;
-  	$render[--$col][$row] = $this->_val;
-  	if ($this->_right->val() !== null) {
-  		$this->_right->draw($render, $val_width, --$col, $row+1);
-  		$col++;
-  	}
-  }
-
   /**
    * Magic toString function
    * @access public
@@ -273,52 +256,6 @@ class BinarySearchTree {
   	}
   }
 
-  /*
-   * Draw the tree
-   */
-  public function draw() {
-  	if ($this->_root !== $this->_sentinel) {
-  		$render_array = array();
-  		$width = 0;
-  		$rows = $this->height();
-  		
-  		$this->_root->draw($render_array, $width);
-  		$edge_line = "";
-  		for ($r = 0; $r < $rows; $r++) {
-  			$node_line = "";
-  			$edge_line = "";
-  			foreach ($render_array as $col=>$column) {
-  				if (isset($column[$r])) {
-  					$node_line .= sprintf("%${width}s", $column[$r]);
-  					$edges = "";
-  					if (isset($render_array[$col+1][$r+1])) {
-  						if ($column[$r] < $this->_root->val() && $render_array[$col+1][$r+1] < $this->_root->val()) {
-	  						$edges .= "/";
-	  					}
-	  					if ($column[$r] > $this->_root->val() && $render_array[$col+1][$r+1] > $this->_root->val()) {
-	  						$edges .= "/";
-	  					}
-  					}
-  					if (isset($render_array[$col-1][$r+1]) && $render_array[$col-1][$r+1] > $this->_root->val()) {
-  						if ($column[$r] < $this->_root->val() && $render_array[$col-1][$r+1] < $this->_root->val()) {
-	  						$edges .= "\\";
-	  					}
-	  					if ($column[$r] > $this->_root->val() && $render_array[$col-1][$r+1] > $this->_root->val()) {
-	  						$edges .= "\\";
-	  					}
-  					}
-  					$edge_line .= sprintf("%-${width}s", $edges);
-  				} else {
-  					$node_line .= sprintf("%${width}s", "");
-  					$edge_line .= sprintf("%${width}s", "");
-  				}
-  			}
-  			echo "$node_line\n";
-  			echo "$edge_line\n";
-  		}
-  	}
-  }
-
 }
 
 $BST = new BinarySearchTree;
@@ -338,9 +275,6 @@ $BST->insert(9);
 $BST->traverse();
 
 echo "\nHeight: ", $BST->height();
-echo "\n\n";
-
-$BST->draw();
 
 exit;
 
